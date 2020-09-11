931d001f66b2851db5a70bac2ce5391f99d41d5e
Colby Hoke <choke@redaht.com>
2020-09-11 17:47:57 -0400

    Syncing after I got Enter button working

--- UFO-level-2/js/app.js
+++ UFO-level-2/js/app.js
@@ -5,6 +5,7 @@ var ufoData = data;
  * Select everything needed
  * ADD DOCUMENTATION
  * 
+ * 
  */
 // Select the button
 var button = d3.select("#filter-btn");
@@ -15,42 +16,27 @@ var form = d3.select("#form");
 // Select the table body
 var tbody = d3.select("tbody");
 
+
 /**
- * INITIAL STATE
+ * Initial state
  * ADD DOCUMENTATION
  * 
  * 
  */
 function init(){
     // Dynamically fill in the table from data.js
-    ufoData.forEach((ufoSightings) => {
+    ufoData.forEach(function(ufoSightings){
     
         // Check output
         //console.log(ufoSightings)
         var row = tbody.append("tr");
 
-        Object.entries(ufoSightings).forEach(([key, value]) => {
+        Object.entries(ufoSightings).forEach(function([key, value]) {
             // Check output
             //console.log(key, value);
 
-            // Make the table data standardized (format it)
-            if (key === "state" || key === "country"){
-                value = value.toUpperCase();
-            };
-
-            // Capitalize city names
-            // Solution from StackOverflow users nimeresam and Cristian Traìna
-            // Source:
-            // https://stackoverflow.com/questions/32589197/how-can-i-capitalize-the-first-letter-of-each-word-in-a-string-using-javascript
-            if (key === "city"){
-                value = value.replace(/(^\w{1})|(\s+\w{1})/g, match => match.toUpperCase());
-            };
-
-            // Add handler to clean up the data in duration and comments columns
-
             // Make a new row for each entry
             var cell = row.append("td");
-            
             // Fill in cell values
             cell.text(value);
         });
@@ -60,34 +46,27 @@ function init(){
 // Event handlers to filter search
 button.on("click", filterSearch);
 form.on("submit", filterSearch);
+d3.selectAll("#selector").on("change", filterByDropdown);
 
-/**
- * Handle the Enter button being pressed
- * Adapted from StackOverflow user TryingToImprove
- * Source: https://stackoverflow.com/questions/13987300/how-to-capture-enter-key-press
- * Combined with: https://keycode.info
- */
-function handle(e){
-    if(e.key === "Enter"){
-        e.preventDefault(); // Prevent page from refreshing
-        filterSearch(); // Call filterSearch function
-    };
-};
+filterByDropdown
+
+
+
+
+
+
 
 // Filter the results, based on date in input field
 function filterSearch(){
 
     // Prevent page from refreshing
-    if (!d3.event == null){
-        d3.event.preventDefault();
-    };
-    
-    /**
-     * Select the input element
-     * Get raw HTML node
-     * Get the value
-     */
+    d3.event.preventDefault();
+
+    // Select the input element & get raw HTML node
     var inputElement = d3.select(".form-control");
+    //console.log(inputElement)
+
+    // Get the value
     var inputValue = inputElement.property("value");
 
     // Check output (uncomment to see)
@@ -97,14 +76,15 @@ function filterSearch(){
     // Filter the data
     var filteredData = ufoData.filter(ufoData => ufoData.datetime === inputValue);
 
-    // Check final output before writing to page (uncomment to see)
-    //console.log(filteredData);
+    console.log(filteredData);
 
     // Clear the table to fill with new values
     tbody.html("");
 
-    filteredData.forEach((ufoSightingsFiltered) => {
-        var row = tbody.append("tr"); // Add a row to the table per 
+    filteredData.forEach(function(ufoSightingsFiltered) {
+        
+        
+        var row = tbody.append("tr");
 
         Object.entries(ufoSightingsFiltered).forEach(function([key, value]) {
             /*console.log(key, value);*/
@@ -121,3 +101,5 @@ function filterSearch(){
 
 // Load default state = show all
 init();
+
+
